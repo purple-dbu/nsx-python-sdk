14acfc7680a1eeed4764d48aa28de2928c715dab
Remi Vichery <rvichery.ext@orange.com>
2014-12-11 14:29:04 +0100

    NSX SDK release 0.1.1

--- nsxsdk/__init__.py
+++ nsxsdk/__init__.py
@@ -3,3 +3,4 @@
 from . import edge
 from . import utils
 from . import firewall
+from . import logicalswitches
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='nsxsdk',
-    version='0.1.0',
+    version='0.1.1',
     description='NSX for vSphere SDK',
     long_description=readme + '\n\n' + doclink + '\n\n' + history,
     author='Remi Vichery',
